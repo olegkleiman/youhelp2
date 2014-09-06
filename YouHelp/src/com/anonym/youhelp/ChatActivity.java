4745a6d5c30b796e3aa80dfa0c979b3897332a5f
Oleg Kleiman <oleg_kleyman@hotmail.com>
2014-09-06 19:27:27 +0300

    Improved support for images in messages

--- YouHelp/src/com/anonym/youhelp/ChatActivity.java
+++ YouHelp/src/com/anonym/youhelp/ChatActivity.java
@@ -1,7 +1,12 @@
 package com.anonym.youhelp;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
@@ -16,12 +21,18 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -41,6 +52,7 @@ public class ChatActivity extends Activity {
 	private ChatAdapter chatAdapter;
 	
 	ImageView profilePictureView = null;
+	ImageView hazardPictureView = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,14 +69,20 @@ public class ChatActivity extends Activity {
 				if( requestingIntent.hasExtra("provider") ) {
 					provider = requestingIntent.getStringExtra("provider");
 				}
+				
+				profilePictureView = (ImageView)findViewById(R.id.imageProfilePicture);
+				GetProfileTask task = new GetProfileTask(); 
+				task.execute(toUserid); 
+				
+				hazardPictureView = (ImageView)findViewById(R.id.imageViewHazardPic);
+				GetBlobTask blobTask = new GetBlobTask(this);
+				blobTask.execute("pictures", "1475946282659875_20140905_215401-888053217.jpg");
 		}
 		
 		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 		myUserID = sharedPrefs.getString("prefUsername", "");
 		
-		profilePictureView = (ImageView)findViewById(R.id.imageProfilePicture);
-		GetProfileTask task = new GetProfileTask(); 
-		task.execute(toUserid); 
+
 		
 		ListView messagesList = (ListView)findViewById(R.id.lvChatRoom);
 		
@@ -201,6 +219,93 @@ public class ChatActivity extends Activity {
 
 		}
 	}
+	
+	private class GetBlobTask extends AsyncTask<String, String, ByteArrayOutputStream>
+	{
+		Context context;
+		Exception error;
+		
+		public static final String storageConnectionString = 
+			    "DefaultEndpointsProtocol=http;" + 
+			    "AccountName=youhelpstorage;" + 
+			    "AccountKey=dtpTqukoGje8FSnSvUBc/of+6Y3FQZRi7eS2+PTanCnAglBBExnsvXjxTjZQxiROUWJbZZijlZ97WR7/l6MDMA==";
+
+		
+		public GetBlobTask(Context ctx){
+			context = ctx;
+		}
+		
+		@Override
+	    protected void onPostExecute(ByteArrayOutputStream result) {
+			try{
+			if( error != null ) {
+            	Toast.makeText(context, error.getMessage(), 
+    						Toast.LENGTH_LONG).show();
+			}else if( result != null ) {
+
+				InputStream decodedInput = new ByteArrayInputStream(result.toByteArray());
+				
+//   			 	// Get the dimensions of the View
+//    		    int targetW = hazardPictureView.getWidth();
+//    		    int targetH = hazardPictureView.getHeight();
+//    		    
+//    		    // Get the dimensions of the bitmap
+//    		    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//    		    bmOptions.inJustDecodeBounds = true;
+//    		    Bitmap bitmap = BitmapFactory.decodeStream(decodedInput, bmOptions);
+    		    
+				
+				Bitmap bitmap = BitmapFactory.decodeStream(decodedInput);
+				hazardPictureView.setImageBitmap(bitmap);
+				
+				//Drawable d = Drawable.createFromStream(decodedInput,"src");
+				//hazardPictureView.setImageDrawable(d);
+
+			}
+			} catch(OutOfMemoryError e){
+				Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+			} catch (Exception ex) {
+				Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
+			}
+		}
+		
+		@Override
+		protected ByteArrayOutputStream doInBackground(String... params) {
+			try {
+				
+				String containerName = params[0];
+				String blobName = params[1];
+				
+				// Retrieve storage account from connection-string.
+			    CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+				
+			    // Create the blob client.
+			    CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+			    
+			    // Retrieve reference to a previously created container.
+			    CloudBlobContainer container = blobClient.getContainerReference(containerName);
+			    
+			    CloudBlockBlob blob = container.getBlockBlobReference(blobName);
+			    
+			    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			    blob.download(baos);
+			    
+			    // Using pipelines seems to be more effective
+//			    PipedOutputStream po = new PipedOutputStream();
+//			    PipedInputStream pi = new PipedInputStream(po);
+//			    pi.read(baos.toByteArray());
+//			    return pi;
+			    
+			    return baos;
+			    
+			} catch(Exception e) {
+		    	error = e;
+		    	return null;
+			}
+
+		}
+		
+	}
 
 	private class GetProfileTask extends AsyncTask<String, Object, Bitmap> 
 	{ 
@@ -221,18 +326,18 @@ public class ChatActivity extends Activity {
 			
 			try{
 			
-			String userid = params[0];
-			
-			fbAvatarUrl = new URL("https://graph.facebook.com/"+userid+"/picture?type=normal");
-			fbAvatarBitmap = BitmapFactory.decodeStream(fbAvatarUrl.openConnection().getInputStream());
-			
-			}catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			return fbAvatarBitmap;
+				String userid = params[0];
+				
+				fbAvatarUrl = new URL("https://graph.facebook.com/"+userid+"/picture?type=normal");
+				fbAvatarBitmap = BitmapFactory.decodeStream(fbAvatarUrl.openConnection().getInputStream());
+				
+				}catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				return fbAvatarBitmap;
 			}
 		}
 	
